6ad7dd28f73409c8c09854d1ca2971f95518ab1e
juan garassino <juan.garassino@hotmail.com>
2022-06-07 14:41:22 +0200

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='image-waves',
+setup(name='imageWaves',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
